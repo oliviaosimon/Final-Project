0300550805bd9c8048e7eccb589f12571ee9567d
oliviaosimon <42871888+oliviaosimon@users.noreply.github.com>
2019-01-01 22:55:38 -0500

    Update pong.py

--- pong.py
+++ pong.py
@@ -1,1 +1,38 @@
+#pong.py
+#Olivia Simon
+#1.1.19
+#Computer Programmming Period 4
+#other sources: 
+    #(platformer) https://github.com/oliviaosimon/Platformer/blob/master/platformer.py
+    
+
+
+from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset, ImageAsset, Frame
+
+myapp = App()
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 800
+
+lightBlue = Color(0x2EFEC8, 1.0)
+black = Color(0x000000, 1.0)
+pink = Color(0xFF00FF, 1.0)
+red = Color(0xFF5733, 1.0)
+white = Color(0xFFFFFF, 1.0)
+red = Color(0xff0000, 1.0)
+green = Color(0x00ff00, 1.0)
+darkBlue = Color(0x0000ff, 1.0)
+black = Color(0x000000, 1.0)
+white = Color(0xffffff, 1.0)
+grey = Color(0xC0C0C0, 1.0)
+
+thinline = LineStyle(2, black)
+blkline = LineStyle(1, black)
+noline = LineStyle(0, white)
+coolline = LineStyle(1, grey)
+blueline = LineStyle(2, darkBlue)
+redline = LineStyle(1, red)
+greenline = LineStyle(1, green)
+gridline = LineStyle(1, grey)
+grid=RectangleAsset(30,30,gridline,white)
 

b1a2ce52850d365b84bae95e336db401ed126148
oliviaosimon <42871888+oliviaosimon@users.noreply.github.com>
2019-01-11 10:54:51 -0500

    Updated from Brython Server: ‎1‎/‎11‎/‎2019 ‎10‎:‎54‎:‎53‎ ‎AM

--- pongongongish.py
+++ pongongongish.py
@@ -57,9 +57,16 @@ class Ball(Sprite):
         rollypolly = CircleAsset(7, thinline, white)
         #myapp.listenKeyEvent('keydown', 'space', self.spaceKey)
         super().__init__(rollypolly, position)
+        Ball.direction = 1
+        Ball.go = True
 
     def step(self):
         global myapp
+        if Ball.go:
+            Ball.x += Ball.direction
+        if Ball.x + Ball.width > myapp.width or ball.x < 0:
+            Ball.x -= Ball.direction
+            #reverse(Ball)
         self.vy *= 0.99
         self.vx *= 0.99
         self.y += self.vy
